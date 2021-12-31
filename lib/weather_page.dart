d05154092a45983d71e44229830ab265d86c3b94
phamtanphat <phatdroid94@gmail.com>
2021-12-31 23:44:20 +0700

    ui update

--- lib/weather_page.dart
+++ lib/weather_page.dart
@@ -1,19 +1,20 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
-import 'package:flutter_weather_29112021/main.dart';
 
 class WeatherPage extends StatefulWidget {
   late double width;
   late double height;
 
-  static WeatherPage of(BuildContext context){
+  static WeatherPage of(BuildContext context) {
     return context.findAncestorWidgetOfExactType()!;
   }
+
   @override
   _WeatherPageState createState() => _WeatherPageState();
 }
 
 class _WeatherPageState extends State<WeatherPage> {
-
   @override
   Widget build(BuildContext context) {
     widget.width = MediaQuery.of(context).size.width;
@@ -22,15 +23,28 @@ class _WeatherPageState extends State<WeatherPage> {
       appBar: AppBar(
         title: Text("Weather App"),
       ),
-      body: Container(
-        constraints: BoxConstraints.expand(),
-        padding: const EdgeInsets.all(5),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            searchBox(),
-            tempCity()
-          ],
+      body: SafeArea(
+        child: Container(
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Colors.lightBlueAccent,
+              Colors.blue,
+            ],
+          )),
+          constraints: BoxConstraints.expand(),
+          padding: const EdgeInsets.all(5),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              // searchBox(),
+              // Expanded(flex: 5, child: tempCity()),
+              // Expanded(flex: 2, child: detailTemp())
+              Expanded(child: notFoundCity())
+            ],
+          ),
         ),
       ),
     );
@@ -39,22 +53,126 @@ class _WeatherPageState extends State<WeatherPage> {
   Widget searchBox() {
     return TextField(
       decoration: InputDecoration(
-          border: OutlineInputBorder(
-              borderRadius: BorderRadius.all(Radius.circular(10))),
-          hintText: "Input city name",
-          suffixIcon: IconButton(
-              icon: Icon(Icons.search),
-              onPressed: () {}),
+        fillColor: Colors.white,
+        filled: true,
+        border: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(10))),
+        hintText: "Input city name",
+        suffixIcon: IconButton(icon: Icon(Icons.search), onPressed: () {}),
       ),
     );
   }
 
   Widget tempCity() {
     return Column(
-      crossAxisAlignment: CrossAxisAlignment.stretch,
+      mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        Text("31 \u00BA C" , style: TextStyle(color: Colors.yellow , fontSize: widget.width / 8))
+        Text("31 \u00BA C",
+            style: TextStyle(color: Colors.yellow, fontSize: widget.width / 4)),
+        Text("Lagos,NG",
+            style: TextStyle(color: Colors.white, fontSize: widget.width / 10)),
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.network(
+              "https://openweathermap.org/img/wn/04n@2x.png",
+              width: widget.width / 2,
+              height: widget.width / 3,
+              fit: BoxFit.fitWidth,
+            ),
+            SizedBox(
+              width: 5,
+            ),
+            Text("broken clouds",
+                style: TextStyle(
+                    fontSize: widget.width / 22,
+                    fontWeight: FontWeight.bold,
+                    color: Color.fromARGB(255, 107, 106, 102)))
+          ],
+        )
       ],
+    );
+  }
+
+  Widget detailTemp() {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Expanded(
+          child: Card(
+            child: Container(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Image.asset("assets/images/ic_humidity.png"),
+                  Text("1013", style: TextStyle(fontSize: widget.width / 20 ,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.grey[900])),
+                  Text("Humidity",
+                      style: TextStyle(
+                          fontSize: widget.width / 25,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.grey[500])),
+                ],
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Card(
+            child: Container(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Image.asset("assets/images/ic_wind.png"),
+                  Text("1013", style: TextStyle(fontSize: widget.width / 20 ,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.grey[900])),
+                  Text("Wind",
+                      style: TextStyle(
+                          fontSize: widget.width / 25,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.grey[500])),
+                ],
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Card(
+            child: Container(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Image.asset("assets/images/ic_air_pressure.png"),
+                  Text("1013", style: TextStyle(fontSize: widget.width / 20 ,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.grey[900])),
+                  Text("Air pressure",
+                      style: TextStyle(
+                          fontSize: widget.width / 25,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.grey[500])),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+  Widget notFoundCity(){
+    return Container(
+      constraints: BoxConstraints.expand(),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset("assets/images/ic_not_found.png"),
+          Text("Seems like you aren't on earth" , style: TextStyle(fontSize: widget.width / 23 , color: Colors.white)),
+        ],
+      ),
     );
   }
 }
