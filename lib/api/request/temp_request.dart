d4113166df7398f73edd8a86efbcbdc3c4e00244
phamtanphat <phatdroid94@gmail.com>
2022-01-05 20:27:46 +0700

    request

--- lib/api/request/temp_request.dart
+++ lib/api/request/temp_request.dart
@@ -1,14 +1,19 @@
 import 'package:dio/dio.dart';
 import 'package:flutter_weather_29112021/api/dio_client.dart';
 
-class TempRequest{
+class TempRequest {
   late Dio _dio;
 
-  TempRequest(){
+  TempRequest() {
     _dio = DioClient.instance.dio;
   }
 
-  Future tempCityRequest(){
-    return _dio.get("");
+  //data/2.5/weather?appid=86183a23377ed034aef7aad102f43d64&units=metric&q=Hanoi
+  Future tempCityRequest(String cityName) {
+    return _dio.get("/data/2.5/weather", queryParameters: {
+      "appid": "86183a23377ed034aef7aad102f43d64",
+      "units": "metric",
+      "q": cityName
+    });
   }
 }
